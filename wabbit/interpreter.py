69285b95ddf39c5543450dd5712eb81c40fa9726
Ezzeri Esa <ezzeri.esa@gmail.com>
2023-01-12 05:11:30 -0800

    exercises - day 2 up to program 3

--- wabbit/interpreter.py
+++ wabbit/interpreter.py
@@ -1,17 +1,20 @@
-from typing import Union
+from typing import Dict, List, Optional, Tuple, Union
 import dataclasses
 
 from expression import (
-    Expr,
     TypeEnum,
     OperatorEnum,
+    Expr,
     Boolean,
     Float,
+    Name,
+    Assign,
     Grouping,
     Integer,
     Binary,
     Unary,
 )
+from statement import Declaration, Expression, Print, Statem
 
 
 @dataclasses.dataclass
@@ -20,73 +23,178 @@ class Value:
     py_value: Union[bool, float, int]
 
 
-def evaluate(expression: Expr) -> Value:  # type: ignore[return]
+@dataclasses.dataclass
+class Environment:
+    values: Dict[str, Optional[Value]]
+
+
+def init_environment() -> Environment:
+    values: Dict[str, Optional[Value]] = {}
+    return Environment(values)
+
+
+def define(environment: Environment, name: str, value: Optional[Value]) -> Environment:
+    environment.values[name] = value
+    return environment
+
+
+def get(environment: Environment, name: str) -> Optional[Value]:
+    if name in environment.values:
+        return environment.values[name]
+
+    raise Exception("Name not found.")
+
+
+def assign(environment: Environment, name: str, value: Optional[Value]) -> Environment:
+    if name in environment.values:
+        environment.values[name] = value
+        return environment
+
+    raise Exception("Name not found.")
+
+
+@dataclasses.dataclass
+class Interpreter:
+    statements: List[Statem]
+    environment: Environment
+
+
+def init_interpreter(statements: List[Statem]) -> Interpreter:
+    environment = init_environment()
+    return Interpreter(statements, environment)
+
+
+def interpret(interpreter: Interpreter) -> List[str]:
+    result: List[str] = []
+
+    for statement in interpreter.statements:
+        interpreter, individual_result = execute(interpreter, statement)
+        result += individual_result
+
+    return result
+
+
+def execute(  # type: ignore[return]
+    interpreter: Interpreter, statement: Statem
+) -> Tuple[Interpreter, List[str]]:
+    match statement:
+        # TODO: Implement storing of const vs var type.
+        case Declaration(name, _, _, initializer):
+            value = None
+
+            if initializer is not None:
+                environment, value = evaluate(interpreter.environment, initializer)
+
+            interpreter.environment = define(interpreter.environment, name.text, value)
+            return interpreter, []
+
+        case Expression(expression):
+            interpreter.environment, result = evaluate(
+                interpreter.environment, expression
+            )
+
+            if isinstance(result, list):
+                return interpreter, result
+
+            return interpreter, []
+
+        case Print(expression):
+            interpreter.environment, result = evaluate(
+                interpreter.environment, expression
+            )
+
+            if isinstance(result, list):
+                return interpreter, result
+
+            assert result is not None
+            return interpreter, [str(result.py_value or "nil")]
+
+        case _:
+            raise Exception("Exhaustive switch error.")
+
+
+def evaluate(environment: Environment, expression: Expr) -> Tuple[Environment, Optional[Value]]:  # type: ignore[return]
     match expression:
         case Boolean(value):
-            return Value(TypeEnum.BOOL, bool(value))
+            return environment, Value(TypeEnum.BOOL, bool(value))
 
         case Float(value):
-            return Value(TypeEnum.FLOAT, float(value))
+            return environment, Value(TypeEnum.FLOAT, float(value))
 
         case Integer(value):
-            return Value(TypeEnum.INT, int(value))
+            return environment, Value(TypeEnum.INT, int(value))
+
+        case Name(text):
+            return environment, get(environment, text)
+
+        case Assign(name, value):
+            environment, result = evaluate(environment, value)
+            return assign(environment, name.text, result), None
 
         case Binary(left, operator_enum, right):
-            left_eval = evaluate(left)
-            right_eval = evaluate(right)
+            environment, left_eval = evaluate(environment, left)
+            environment, right_eval = evaluate(environment, right)
+            assert left_eval is not None and right_eval is not None
 
             assert left_eval.type == right_eval.type
 
             if operator_enum == OperatorEnum.PLUS:
                 if left_eval.type == TypeEnum.FLOAT:
-                    return Value(
+                    return environment, Value(
                         TypeEnum.FLOAT, left_eval.py_value + right_eval.py_value
                     )
 
                 elif left_eval.type == TypeEnum.INT:
-                    return Value(TypeEnum.INT, left_eval.py_value + right_eval.py_value)
+                    return environment, Value(
+                        TypeEnum.INT, left_eval.py_value + right_eval.py_value
+                    )
 
             elif operator_enum == OperatorEnum.MINUS:
                 if left_eval.type == TypeEnum.FLOAT:
-                    return Value(
+                    return environment, Value(
                         TypeEnum.FLOAT, left_eval.py_value - right_eval.py_value
                     )
 
                 elif left_eval.type == TypeEnum.INT:
-                    return Value(TypeEnum.INT, left_eval.py_value - right_eval.py_value)
+                    return environment, Value(
+                        TypeEnum.INT, left_eval.py_value - right_eval.py_value
+                    )
 
             elif operator_enum == OperatorEnum.TIMES:
                 if left_eval.type == TypeEnum.FLOAT:
-                    return Value(
+                    return environment, Value(
                         TypeEnum.FLOAT, left_eval.py_value * right_eval.py_value
                     )
 
                 elif left_eval.type == TypeEnum.INT:
-                    return Value(TypeEnum.INT, left_eval.py_value * right_eval.py_value)
+                    return environment, Value(
+                        TypeEnum.INT, left_eval.py_value * right_eval.py_value
+                    )
 
             elif operator_enum == OperatorEnum.DIVIDE:
                 if left_eval.type == TypeEnum.FLOAT:
-                    return Value(
+                    return environment, Value(
                         TypeEnum.FLOAT, left_eval.py_value / right_eval.py_value
                     )
 
                 elif left_eval.type == TypeEnum.INT:
-                    return Value(
+                    return environment, Value(
                         TypeEnum.INT, left_eval.py_value // right_eval.py_value
                     )
 
         case Grouping(expression):
-            return evaluate(expression)
+            return evaluate(environment, expression)
 
         case Unary(operator_enum, right):
-            right_eval = evaluate(right)
+            environment, right_eval = evaluate(environment, right)
+            assert right_eval is not None
 
             if operator_enum == OperatorEnum.MINUS:
                 if right_eval.type == TypeEnum.FLOAT:
-                    return Value(TypeEnum.FLOAT, -right_eval.py_value)
+                    return environment, Value(TypeEnum.FLOAT, -right_eval.py_value)
 
                 elif right_eval.type == TypeEnum.INT:
-                    return Value(TypeEnum.INT, -right_eval.py_value)
+                    return environment, Value(TypeEnum.INT, -right_eval.py_value)
 
         case _:
             raise Exception("Exhaustive switch error.")
--- wabbit/test_interpreter.py
+++ wabbit/test_interpreter.py
@@ -1,23 +1,39 @@
-from interpreter import Value, evaluate
-from expression import TypeEnum, OperatorEnum, Integer, Binary, Float, Grouping, Unary
+from interpreter import Value, evaluate, init_environment, init_interpreter, interpret
+from expression import (
+    TypeEnum,
+    OperatorEnum,
+    Integer,
+    Float,
+    Name,
+    Type,
+    Assign,
+    Binary,
+    Grouping,
+    Unary,
+)
+from statement import DeclarationEnum, Declaration, Expression, Print
 
 
-def test_evaluate_primitives():
+def test_evaluate():
+    environment = init_environment()
+
     # program 1
-    assert evaluate(Integer("42")) == Value(TypeEnum.INT, 42)
+    assert evaluate(environment, Integer("42"))[1] == Value(TypeEnum.INT, 42)
 
     # program 2
-    assert evaluate(Binary(Integer("2"), OperatorEnum.PLUS, Integer("3"))) == Value(
-        TypeEnum.INT, 5
-    )
+    assert evaluate(environment, Binary(Integer("2"), OperatorEnum.PLUS, Integer("3")))[
+        1
+    ] == Value(TypeEnum.INT, 5)
     assert evaluate(
+        environment,
         Binary(
             Unary(OperatorEnum.MINUS, Integer("2")),
             OperatorEnum.PLUS,
             Integer("3"),
-        )
-    ) == Value(TypeEnum.INT, 1)
+        ),
+    )[1] == Value(TypeEnum.INT, 1)
     assert evaluate(
+        environment,
         Binary(
             Integer("2"),
             OperatorEnum.PLUS,
@@ -26,19 +42,44 @@ def test_evaluate_primitives():
                 OperatorEnum.TIMES,
                 Unary(OperatorEnum.MINUS, Integer("4")),
             ),
-        )
-    ) == Value(TypeEnum.INT, -10)
+        ),
+    )[1] == Value(TypeEnum.INT, -10)
     assert evaluate(
+        environment,
         Binary(
             Grouping(Binary(Integer("2"), OperatorEnum.PLUS, Integer("3"))),
             OperatorEnum.TIMES,
             Integer("4"),
-        )
-    ) == Value(TypeEnum.INT, 20)
+        ),
+    )[1] == Value(TypeEnum.INT, 20)
     assert evaluate(
+        environment,
         Binary(
             Float("2.0"),
             OperatorEnum.MINUS,
             Binary(Float("3.0"), OperatorEnum.DIVIDE, Float("4.0")),
-        )
-    ) == Value(TypeEnum.FLOAT, 1.25)
+        ),
+    )[1] == Value(TypeEnum.FLOAT, 1.25)
+
+    # program 3
+    statements = [
+        Declaration(Name("pi"), DeclarationEnum.CONST, None, Float("3.14159")),
+        Declaration(
+            Name("tau"),
+            DeclarationEnum.CONST,
+            None,
+            Binary(Float("2.0"), OperatorEnum.MINUS, Name("pi")),
+        ),
+        Declaration(Name("radius"), DeclarationEnum.VAR, None, Float("4.0")),
+        Declaration(Name("perimeter"), DeclarationEnum.VAR, Type(TypeEnum.FLOAT), None),
+        Expression(
+            Assign(
+                Name("perimeter"),
+                Binary(Name("tau"), OperatorEnum.TIMES, Name("radius")),
+            )
+        ),
+        Print(Name("perimeter")),
+    ]
+
+    interpreter = init_interpreter(statements)
+    assert interpret(interpreter) == ["-4.5663599999999995"]
