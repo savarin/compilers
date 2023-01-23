17e541be23f42f22c3312689208395f9ae52b951
Ezzeri Esa <ezzeri.esa@gmail.com>
2023-01-23 12:00:26 -0800

    exercises - day 3 parser up to program 4

--- wabbit/test_parser.py
+++ wabbit/test_parser.py
@@ -25,3 +25,24 @@ def test_parse():
     assert isinstance(statements[0], Print)
     assert isinstance(statements[0].expression, Integer)
     assert statements[0].expression.value == "1"
+
+    # program 4
+    statements = source_to_statements(
+        """\
+print 1;
+print 2;
+print 3;
+"""
+    )
+
+    assert isinstance(statements[0], Print)
+    assert isinstance(statements[0].expression, Integer)
+    assert statements[0].expression.value == "1"
+
+    assert isinstance(statements[1], Print)
+    assert isinstance(statements[1].expression, Integer)
+    assert statements[1].expression.value == "2"
+
+    assert isinstance(statements[2], Print)
+    assert isinstance(statements[2].expression, Integer)
+    assert statements[2].expression.value == "3"
