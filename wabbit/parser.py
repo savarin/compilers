b2467726f1ab70a917e08c48c7cc90be6cf52100
Ezzeri Esa <ezzeri.esa@gmail.com>
2023-01-23 11:30:22 -0800

    exercises - day 3 parser up to program 2

--- wabbit/parser.py
+++ wabbit/parser.py
@@ -2,7 +2,11 @@ from typing import List, Tuple
 import dataclasses
 
 import scanner
-from statement import Statem, Break
+from statement import Statem, Break, Continue
+
+
+class ParseError(Exception):
+    ...
 
 
 @dataclasses.dataclass
@@ -19,14 +23,58 @@ def parse(parser: Parser) -> List[Statem]:
     statements: List[Statem] = []
 
     while not is_at_end(parser):
-        parser, individual_statement = break_statement(parser)
+        parser, individual_statement = statement(parser)
         statements.append(individual_statement)
 
     return statements
 
 
+def statement(parser: Parser) -> Tuple[Parser, Statem]:
+    parser, is_break = match(parser, [scanner.TokenType.BREAK])
+
+    if is_break:
+        return break_statement(parser)
+
+    parser, is_continue = match(parser, [scanner.TokenType.CONTINUE])
+
+    if is_continue:
+        return continue_statement(parser)
+
+    raise ParseError()
+
+
 def break_statement(parser: Parser) -> Tuple[Parser, Statem]:
-    return advance(parser), Break()
+    parser = consume(
+        parser, scanner.TokenType.SEMICOLON, "Expect ';' after break statement."
+    )
+
+    return parser, Break()
+
+
+def continue_statement(parser: Parser) -> Tuple[Parser, Statem]:
+    parser = consume(
+        parser, scanner.TokenType.SEMICOLON, "Expect ';' after continue statement."
+    )
+
+    return parser, Continue()
+
+
+def match(parser: Parser, token_types: List[scanner.TokenType]) -> Tuple[Parser, bool]:
+    for token_type in token_types:
+        if expect(parser, token_type):
+            parser = advance(parser)
+            return parser, True
+
+    return parser, False
+
+
+def consume(
+    parser: Parser, token_type: scanner.TokenType, message: str
+) -> Tuple[Parser, scanner.Token]:
+    if expect(parser, token_type):
+        return advance(parser)
+
+    raise ParseError()
 
 
 def advance(parser: Parser) -> Parser:
@@ -36,5 +84,16 @@ def advance(parser: Parser) -> Parser:
     return parser
 
 
+def expect(parser: Parser, token_type: scanner.TokenType) -> bool:
+    if is_at_end(parser):
+        return False
+
+    return peek(parser).token_type == token_type
+
+
+def peek(parser: Parser) -> scanner.Token:
+    return parser.tokens[parser.current]
+
+
 def is_at_end(parser: Parser) -> bool:
-    return parser.tokens[parser.current].token_type == scanner.TokenType.EOF
+    return peek(parser).token_type == scanner.TokenType.EOF
--- wabbit/test_parser.py
+++ wabbit/test_parser.py
@@ -2,7 +2,7 @@ from typing import List
 
 from parser import init_parser, parse
 from scanner import init_scanner, scan
-from statement import Statem, Break
+from statement import Statem, Break, Continue
 
 
 def source_to_statements(source: str) -> List[Statem]:
@@ -14,3 +14,7 @@ def test_parse():
     # program 1
     statements = source_to_statements("break;")
     assert isinstance(statements[0], Break)
+
+    # program 2
+    statements = source_to_statements("continue;")
+    assert isinstance(statements[0], Continue)
