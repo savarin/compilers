60dee2f8969e3d464af037a091c8ccb879aad59a
Ezzeri Esa <ezzeri.esa@gmail.com>
2023-01-23 09:48:25 -0800

    exercises - day 3 scanner up to program 7

--- wabbit/test_scanner.py
+++ wabbit/test_scanner.py
@@ -253,3 +253,58 @@ while x < 11 {
     assert tokens[31].token_type == TokenType.SEMICOLON
     assert tokens[32].token_type == TokenType.RIGHT_BRACE
     assert tokens[33].token_type == TokenType.EOF
+
+    # program 7
+    tokens = scan(
+        init_scanner(
+            """\
+var n = 5;
+while true {
+    if n == 0 {
+        break;
+    } else {
+        print n;   
+        n = n - 1;
+        continue;
+    }
+    n = n + 1;
+"""
+        )
+    )
+    assert tokens[0].token_type == TokenType.VAR
+    assert tokens[1].token_type == TokenType.IDENTIFIER
+    assert tokens[2].token_type == TokenType.EQUAL
+    assert tokens[3].token_type == TokenType.NUMBER
+    assert tokens[4].token_type == TokenType.SEMICOLON
+    assert tokens[5].token_type == TokenType.WHILE
+    assert tokens[6].token_type == TokenType.TRUE
+    assert tokens[7].token_type == TokenType.LEFT_BRACE
+    assert tokens[8].token_type == TokenType.IF
+    assert tokens[9].token_type == TokenType.IDENTIFIER
+    assert tokens[10].token_type == TokenType.EQUAL_EQUAL
+    assert tokens[11].token_type == TokenType.NUMBER
+    assert tokens[12].token_type == TokenType.LEFT_BRACE
+    assert tokens[13].token_type == TokenType.IDENTIFIER
+    assert tokens[14].token_type == TokenType.SEMICOLON
+    assert tokens[15].token_type == TokenType.RIGHT_BRACE
+    assert tokens[16].token_type == TokenType.IDENTIFIER
+    assert tokens[17].token_type == TokenType.LEFT_BRACE
+    assert tokens[18].token_type == TokenType.PRINT
+    assert tokens[19].token_type == TokenType.IDENTIFIER
+    assert tokens[20].token_type == TokenType.SEMICOLON
+    assert tokens[21].token_type == TokenType.IDENTIFIER
+    assert tokens[22].token_type == TokenType.EQUAL
+    assert tokens[23].token_type == TokenType.IDENTIFIER
+    assert tokens[24].token_type == TokenType.MINUS
+    assert tokens[25].token_type == TokenType.NUMBER
+    assert tokens[26].token_type == TokenType.SEMICOLON
+    assert tokens[27].token_type == TokenType.IDENTIFIER
+    assert tokens[28].token_type == TokenType.SEMICOLON
+    assert tokens[29].token_type == TokenType.RIGHT_BRACE
+    assert tokens[30].token_type == TokenType.IDENTIFIER
+    assert tokens[31].token_type == TokenType.EQUAL
+    assert tokens[32].token_type == TokenType.IDENTIFIER
+    assert tokens[33].token_type == TokenType.PLUS
+    assert tokens[34].token_type == TokenType.NUMBER
+    assert tokens[35].token_type == TokenType.SEMICOLON
+    assert tokens[36].token_type == TokenType.EOF
